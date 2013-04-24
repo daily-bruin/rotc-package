0a31f44370b47a9084102ef1d96fe7ee6cd1718b
omniaura <omniaura5@gmail.com>
2013-04-24 00:02:54 -0700

    landing page, integrated timeline

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*
+$(document).ready(function() {
+        console.log(parseFloat($('.timelinePhoto').css('left')));
+        $('#next').click(function() {
+            console.log(parseFloat($('.timelinePhoto').css('left')));
+            if (parseFloat($('.timelinePhoto').css('left')) < 1000) {
+                $('.timelinePhoto').animate({left: '-=750'}, 1000);
+            }
+    });
+    
+});
+*/
+
+
+
+var generateScroller = function(media_root, content, photo_width) {
+    scroller_pos = 0;
+    content_width = (content.length - 1) * photo_width
+    document.write(
+        '<div id="scroller">'+
+            '<div id="next">Next&raquo;</div>'+
+            '<div id="passepartout"></div>'+
+            '<!--<div id="timelineHeadDecoration"></div>-->'
+    );
+    for (i=0; i < content.length; i++) {
+        document.write(
+            '<div class="timelinePhoto">'+
+                '<div class="timelineHead" id="timelineHead-'+(i+1)+'">'+
+                    '<span class="time">'+content[i][1]+'</span>'+
+                    '<span class="activity">'+content[i][2]+'</span>'+
+                '</div>'+
+                '<img src="'+media_root+content[i][0]+'"/>'+
+            '</div>'
+        )
+    };
+    document.write('</div>');
+
+    $('#next').click(function() {
+        if (scroller_pos < content.length - 2) {
+            $('.timelinePhoto').animate({left: '-='+photo_width}, 1000);
+            scroller_pos++;
+        }
+        else if (scroller_pos === content.length - 2) {
+            $('#next').html("&laquo;Back");
+            $('.timelinePhoto').animate({left: '-='+photo_width}, 1000);
+            scroller_pos++;
+        }
+        else {
+            $('#next').html("Next&raquo;");
+            $('.timelinePhoto').animate({left: '+='+content_width}, 1000);
+            scroller_pos = 0;
+        }
+    });
+}
